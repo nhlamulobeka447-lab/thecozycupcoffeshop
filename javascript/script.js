e718d759b0ebcb85cb12ba532649e933a9f8f72d
nhlamulobeka447-lab <nhlamulobeka447@gmail.com>
2026-06-19 21:43:04 +0200

    new css changes

--- javascript/script.js
+++ javascript/script.js
@@ -73,3 +73,11 @@ document.querySelectorAll("button").forEach(btn => {
     }, 120);
   });
 });
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.style.opacity = 0;
+  document.body.style.transition = "opacity 0.8s ease-in-out";
+
+  setTimeout(() => {
+    document.body.style.opacity = 1;
+  }, 50);
+});
